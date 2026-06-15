97713a73dba18c63fe3b6f6774413f8549213445
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-06-15 10:57:24 +0530

    Added data types demo program

---
+++ JavaBasics/DataTypesDemo.java
@@ -0,0 +1,16 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        float height = 5.4f;
+        char grade = 'A';
+        boolean student = true;
+        String name = "Sravya";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + student);
+    }
+}
