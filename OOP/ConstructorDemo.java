9c68488f3f2b7e7e02d8861e517542220c72278a
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-07-01 12:25:55 +0530

    Add constructor example

---
+++ OOP/ConstructorDemo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Student {
+
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class ConstructorDemo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = sc.nextInt();
+
+        Student s = new Student(name, age);
+
+        s.display();
+
+        sc.close();
+    }
+}
