359c47b56b2d4642e4fb112036017f8a371bd0b2
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-07-18 23:30:39 +0530

    Add longest word in sentence program

---
+++ Strings/LongestWordInSentence.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LongestWordInSentence {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split(" ");
+
+        String longest = words[0];
+
+        for (int i = 1; i < words.length; i++) {
+
+            if (words[i].length() > longest.length()) {
+                longest = words[i];
+            }
+        }
+
+        System.out.println("\nLongest Word: " + longest);
+        System.out.println("Length: " + longest.length());
+
+        sc.close();
+    }
+}
